d97269eb74837e7b304b782054df6abcdbb1d3a3
Ricardo Bedin <ricardo.bedin@gmail.com>
2023-09-16 16:49:27 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/stripe-webhook"],
+  ignoredRoutes: ["/api/stripe-webhook"],
 });
 
 export const config = {
